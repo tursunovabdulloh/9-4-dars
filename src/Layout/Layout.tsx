07ac4384e704d52a1c4b43cbd7732af9ed4ac396
Abdulloh <email@example.com>
2024-09-23 19:14:46 +0500

    done

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import FormSection from "./FormSection";
+// import FormSection from "./FormSection";
 
 export default function layout() {
   return (
